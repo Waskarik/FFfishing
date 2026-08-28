f5da1bd1724191a6f5e7b6ea6e59bd3ff5a195df
Leanderson Costa <exmoser@gmail.com>
2026-08-28 08:20:46 +0100

    last minute stuff css

--- BadFish/src/App.jsx
+++ BadFish/src/App.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import TrackerPage from "./pages/TrackerPage";
 import About from "./pages/About";
-import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/tracker" element={<TrackerPage />} />
       <Route path="/about" element={<About />} />
-      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
